ed5b69d009ff446a3659463bc14f1a35d2eeb87f
Juan Pablo Millicay <juan22milli@gmail.com>
2026-04-24 13:50:55 -0300

    se modifico para centralizar las validaciones para ambas vistas de agregar y editar

--- middlewares/validationMiddleware.mjs
+++ middlewares/validationMiddleware.mjs
@@ -1,36 +1,25 @@
 import { validationResult } from "express-validator";
 
-// Middleware final para capturar errores Agregar
-  export const validarAgregar = (req, res, next) => {
-        const errores = validationResult(req);  // <--- Captura resultados
-        //<--- Verifica si hay errores 
-        if (!errores.isEmpty()) {      
-          // envia para una sola vista EJS          
-            return res.status(400).render("addSuperhero", {
-              errores: errores.array(),
-              hero: req.body
-    });
-        }
-        // Procesar datos si no hay errores...
-        next();
-    }   
+export const validar = (vista) => {
+  return (req, res, next) => {
+    const errores = validationResult(req);
+// Verifica si hay errores 
+    if (!errores.isEmpty()) {
+      // envia a la vista EJS definida en el controlador
+      return res.status(400).render(vista, {
+        errores: errores.array(),
 
+        // 👇 datos del formulario (sirve para no perder lo escrito)
+        hero: {
+          ...req.body,
+          _id: req.params.id || null
+        },
 
-    // Middleware final para capturar errores Editar
-  export const validarEditar = (req, res, next) => {
-        const errores = validationResult(req);  // <--- Captura resultados
-        //<--- Verifica si hay errores 
-        if (!errores.isEmpty()) {      
-          // envia para una sola vista EJS          
-            return res.status(400).render("editSuperhero", {
-              errores: errores.array(),
-              //datos: req.body - hero is not defined
-                hero: {
-                   ...req.body,
-                   _id: req.params.id   // 👈 agregás el id
-                 }
-    });
-        }
-        // Procesar datos si no hay errores...
-        next();
-    }   
+        // 👇 por si quiero usar el id separado en la vista
+        //id: req.params.id || null
+      });
+    }
+    // Procesar datos si no hay errores..
+    next();
+  };
+};
